e93d556fc08ab3aa915b669ee1d6dc4879e21ddc
Jorge Nascimento <jorgeinhoparaiba@gmail.com>
2025-01-28 09:14:22 -0300

    aprendendo JS

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}km/h`
+)
+
+if (vel > 60){ //condição simples
+    console.log("Você ultrapassou o limite de velocidade, MULTADO")
+}
+console.log(`Dirija sempre com cuidado`)
+
